c21f93266bf3cd8dfa99404e580a0c7ebdf4eaed
sujianduo <sujianduo@gmail.com>
2025-05-26 14:30:22 +0800

    Add files via upload

--- kls1.js
+++ kls1.js
@@ -762,13 +762,13 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/CachedBmp_11.png?1748238271143", id:"CachedBmp_11"},
-		{src:"images/IMG_20250430_101050.jpg?1748238271143", id:"IMG_20250430_101050"},
-		{src:"images/kls1_atlas_1.png?1748238271065", id:"kls1_atlas_1"},
-		{src:"sounds/dui.mp3?1748238271143", id:"dui"},
-		{src:"sounds/cuo.mp3?1748238271143", id:"cuo"},
-		{src:"sounds/wan.mp3?1748238271143", id:"wan"},
-		{src:"sounds/dao.mp3?1748238271143", id:"dao"}
+		{src:"images/CachedBmp_11.png?1748240726242", id:"CachedBmp_11"},
+		{src:"images/IMG_20250430_101050.jpg?1748240726242", id:"IMG_20250430_101050"},
+		{src:"images/kls1_atlas_1.png?1748240726157", id:"kls1_atlas_1"},
+		{src:"sounds/dui.mp3?1748240726242", id:"dui"},
+		{src:"sounds/cuo.mp3?1748240726242", id:"cuo"},
+		{src:"sounds/wan.mp3?1748240726242", id:"wan"},
+		{src:"sounds/dao.mp3?1748240726242", id:"dao"}
 	],
 	preloads: []
 };
